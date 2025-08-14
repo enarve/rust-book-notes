b388102c26e7c11715148543bdc381e815200b70
Nikita Vlaskin <sinezeleuny@gmail.com>
2025-08-14 15:45:20 +0200

    Methods

--- 05_structures/rectangles/src/main.rs
+++ 05_structures/rectangles/src/main.rs
@@ -4,22 +4,65 @@ struct Rectangle {
     height: u32,
 }
 
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn width(&self) -> bool {
+        self.width > 0
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
+impl Rectangle {
+    fn square(size: u32) -> Self {
+        Self {
+            width: size,
+            height: size,
+        }
+    }
+}
+
 fn main() {
+
+let sq = Rectangle::square(3);
 
     let scale = 2;
     let rect1 = Rectangle {
-        width: dbg!(30*scale),
+        width: /*dbg!(30*scale)*/ 30,
         height: 50,
     };
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
+
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
 
     dbg!(&rect1);
 
     // println!("rect1 is {:#?}", rect1);
 
-    // println!(
-    //     "The area of the rectangle is {} square pixels.",
-    //     area(&rect1)
-    // );
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        rect1.area()
+    );
+
+    if rect1.width() {
+        println!(
+            "The rectangle has a nonzero width; it is {}",
+            rect1.width
+        );
+    }
 }
 
 fn area(rectangle: &Rectangle) -> u32 {
