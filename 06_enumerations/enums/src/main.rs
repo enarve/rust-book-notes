42abb015f583f65664a88d9453742afde3341912
Nikita Vlaskin <sinezeleuny@gmail.com>
2025-08-16 00:33:54 +0200

    Match

--- 06_enumerations/enums/src/main.rs
+++ 06_enumerations/enums/src/main.rs
@@ -40,6 +40,16 @@ fn main() {
 
     let m = Message::Write(String::from("hello"));
     m.call();
+
+    let some_number = Some(5);
+    let some_char = Some('e');
+
+    let absent_number: Option<i32> = None;
+
+    let x: i8 = 5;
+    let y: Option<i8> = Some(5);
+
+    // let sum = x + y;
 }
 
 fn route(ip_kind: IpAddr) {}
