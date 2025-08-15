752bce06af1843b74672169150dd44914c1905f1
Nikita Vlaskin <sinezeleuny@gmail.com>
2025-08-15 23:21:31 +0200

    More on enums

--- 06_enumerations/enums/src/main.rs
+++ 06_enumerations/enums/src/main.rs
@@ -1,31 +1,45 @@
-enum IpAddrKind {
-    V4,
-    V6,
+enum Message {
+    Quit,
+    Move {x: i32, y: i32},
+    Write(String),
+    ChangeColor(i32, i32, i32),
 }
 
-struct IpAddr {
-    kind: IpAddrKind,
-    address: String,
+impl Message {
+    fn call(&self) {
+        // method body would be defined here
+    }
 }
+
+enum IpAddr {
+    V4(u8, u8, u8, u8),
+    V6(String),
+}
+
+// struct IpAddr {
+//     kind: IpAddrKind,
+//     address: String,
+// }
 
 fn main() {
-    let four = IpAddrKind::V4;
-    let six = IpAddrKind::V6;
+    let four = IpAddr::V4(127, 0, 0, 1);
+    let six = IpAddr::V6(String::from("::1"));
 
     route(four);
     route(six);
 
-    let home = IpAddr {
-        kind: IpAddrKind::V4,
-        address: String::from("127.0.0.1"),
-    };
+    // let home = IpAddr {
+    //     kind: IpAddrKind::V4,
+    //     address: String::from("127.0.0.1"),
+    // };
 
-    let loopback = IpAddr {
-        kind: IpAddrKind::V6,
-        address: String::from("::1"),
-    };
+    // let loopback = IpAddr {
+    //     kind: IpAddrKind::V6,
+    //     address: String::from("::1"),
+    // };
+
+    let m = Message::Write(String::from("hello"));
+    m.call();
 }
 
-fn route(ip_kind: IpAddrKind) {
-
-}
+fn route(ip_kind: IpAddr) {}
