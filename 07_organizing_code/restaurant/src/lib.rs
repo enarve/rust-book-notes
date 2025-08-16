c8720c4fc5ddbefdb8f25fef1f68eaa595cddc48
Nikita Vlaskin <sinezeleuny@gmail.com>
2025-08-16 17:16:24 +0200

    Use

--- 07_organizing_code/restaurant/src/lib.rs
+++ 07_organizing_code/restaurant/src/lib.rs
@@ -45,24 +45,37 @@ mod back_of_house {
 
 fn deliver_order() {}
 
-pub fn eat_at_restaurant() {
-    // Absolute path
-    crate::front_of_house::hosting::add_to_waitlist();
+pub use crate::front_of_house::hosting;
+use std::collections::HashMap;
+// use std::fmt::Result;
+// use std::io::Result as IoResult;
+// use std::{cmp::Ordering, io};
+// use std::collections::*; // glob operator
 
-    // Relative path
-    front_of_house::hosting::add_to_waitlist();
+// mod customer {
+    pub fn eat_at_restaurant() {
+        // Absolute path
+        // crate::front_of_house::hosting::add_to_waitlist();
 
-    // Order a breakfast in the summer with Rye toast.
-    let mut meal = back_of_house::Breakfast::summer("Rye");
-    // Change our mind about what bread we'd like.
-    meal.toast = String::from("Wheat");
-    println!("I'd like {} toast please", meal.toast);
+        // Relative path
+        // front_of_house::hosting::add_to_waitlist();
 
-    // The next line won't compile if we uncomment it; we're not
-    // allowed to see or modify the seasonal fruit that comes
-    // with the meal.
-    // meal.seasonal_fruit = String::from("blueberries");
+        hosting::add_to_waitlist();
+        let mut map = HashMap::new();
+        map.insert(1, 2);
 
-    let order1 = back_of_house::Appetizer::Soup;
-    let order2 = back_of_house::Appetizer::Salad;
-}
+        // Order a breakfast in the summer with Rye toast.
+        let mut meal = back_of_house::Breakfast::summer("Rye");
+        // Change our mind about what bread we'd like.
+        meal.toast = String::from("Wheat");
+        println!("I'd like {} toast please", meal.toast);
+
+        // The next line won't compile if we uncomment it; we're not
+        // allowed to see or modify the seasonal fruit that comes
+        // with the meal.
+        // meal.seasonal_fruit = String::from("blueberries");
+
+        let order1 = back_of_house::Appetizer::Soup;
+        let order2 = back_of_house::Appetizer::Salad;
+    }
+// }
