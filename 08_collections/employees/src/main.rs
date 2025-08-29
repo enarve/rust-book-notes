6b36058a4ebcdf424706868dfc19297a94838d31
Nikita Vlaskin <sinezeleuny@gmail.com>
2025-08-29 18:38:26 +0200

    Employees implemented

--- 08_collections/employees/src/main.rs
+++ 08_collections/employees/src/main.rs
@@ -5,16 +5,23 @@
 // all people in a department or all people in a company,
 // sorted alphabetically.
 
+// get all
+// get from Engineering
+
 use std::collections::HashMap;
 
 fn main() {
-    let hm: HashMap<String, String> = HashMap::new();
+    let mut hm: HashMap<String, String> = HashMap::new();
 
     loop {
     
         let reader = std::io::stdin();
         let mut buf = String::new();
         let quits = ["q", "quit"];
+        let add_command = "add";
+        let get_command = "get";
+        let all_subcommand = "all";
+        let from_subcommand = "from";
 
         match reader.read_line(&mut buf) {
             Ok(_) => (),
@@ -28,11 +35,41 @@ fn main() {
         
         // Parse else and decide what to do
         let words: Vec<&str> = buf.split(" ").collect();
-        if words.len() == 4 {
-            // Either add to hashmap or print some results
-            println!("To do...");
-        } else {
-            println!("Wrong command.");
+
+        if !words.is_empty() {
+            let command = words[0].trim().to_lowercase();
+                
+            if command == add_command {
+                if words.len() == 4 {
+                    let name = words[1].trim();
+                    let department = words[3].trim();
+                    println!("Adding {name} employee to {department}...");
+                    hm.insert(String::from(name), String::from(department));
+                }
+            } else if command == get_command {
+                if words.len() == 2 {
+                    let subcommand = words[1].trim();
+                    if subcommand == all_subcommand {
+                        // print all
+                        println!("printing all");
+                        for (k, v) in &hm {
+                            println!("{} {}", k, v);
+                        }
+                    }
+                } else if words.len() == 3 {
+                    let subcommand = words[1].trim();
+                    let dep = words[2].trim();
+                    if subcommand == from_subcommand {
+                        // print some
+                        println!("printing some");
+                        for (k, v) in &hm {
+                            if v == dep {
+                            println!("{}", k);
+                            }
+                        }
+                    }
+                }
+            }
         }
     
     }
