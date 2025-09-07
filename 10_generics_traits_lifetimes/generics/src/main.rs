8a69fe2a46767c371a2bb21bd8dbb7e298341c02
Nikita Vlaskin <sinezeleuny@gmail.com>
2025-09-07 22:43:47 +0200

    Generics

--- 10_generics_traits_lifetimes/generics/src/main.rs
+++ 10_generics_traits_lifetimes/generics/src/main.rs
@@ -1,15 +1,42 @@
+struct Point<T> {
+    x: T,
+    y: T,
+}
+
+impl<T> Point<T> {
+    fn x(&self) -> &T {
+        &self.x
+    }
+}
+
+impl Point<f32> {
+    fn distance_from_origin(&self) -> f32 {
+        (self.x.powi(2) + self.y.powi(2)).sqrt()
+    }
+}
+
 fn main() {
+
+    let integer = Point { x: 5, y: 10 };
+    let float = Point { x: 1.0, y: 4.0 };
+
+    println!("integer.x = {}", integer.x());
+
     let number_list = vec![34, 50, 25, 100, 65];
     let result = largest(&number_list);
     println!("The largest number is {result}");
 
-    let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
-    let result = largest(&number_list);
-    println!("The largest number is {result}");
+    // let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
+    // let result = largest(&number_list);
+    // println!("The largest number is {result}");
     
+    let char_list = vec!['y', 'm', 'a', 'q'];
+
+    let result = largest(&char_list);
+    println!("The largest char is {result}");
 }
 
-fn largest(list: &[i32]) -> &i32 {
+fn largest<T: std::cmp::PartialOrd>(list: &[T]) -> &T {
     let mut largest = &list[0];
 
     for item in list {
@@ -20,3 +47,5 @@ fn largest(list: &[i32]) -> &i32 {
 
     largest
 }
+
+// monomorphization of generics, no runtime cost
