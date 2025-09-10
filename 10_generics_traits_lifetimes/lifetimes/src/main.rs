1d40d6d4caea1011c47a564923d039c32554d784
Nikita Vlaskin <sinezeleuny@gmail.com>
2025-09-10 21:57:59 +0200

    More lifetimes

--- 10_generics_traits_lifetimes/lifetimes/src/main.rs
+++ 10_generics_traits_lifetimes/lifetimes/src/main.rs
@@ -1,4 +1,27 @@
+use std::fmt::Display;
+
+struct ImportantExcerpt<'a> {
+    part: &'a str,
+}
+
+impl<'a> ImportantExcerpt<'a> {
+    fn level(&self) -> i32 {
+        3
+    }
+}
+
 fn main() {
+    let novel = String::from(
+        "Call me Ishmael. Some years ago..."
+    );
+    let first_sentence = novel.split('.')
+    .next()
+    .expect("Could not find a '.'");
+
+    let i = ImportantExcerpt {
+        part: first_sentence,
+    };
+    
     // let r;
     // {
     //     let x = 5;
@@ -14,8 +37,11 @@ fn main() {
     {
         let string2 = String::from("xyz");
         result = longest(string1.as_str(), string2.as_str());
+        println!("The longest string is {result}");
     }
-    println!("The longest string is {result}");
+
+    let s: &'static str = "I have a static lifetime.";
+    
 }
 
 fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
@@ -29,3 +55,18 @@ fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
 // &i32
 // &'a i32
 // &'a mut i32
+
+fn longest_with_an_announcement<'a, T>(
+    x: &'a str,
+    y: &'a str,
+    ann: T,
+) -> &'a str
+where T: Display,
+{
+    println!("Announcement! {ann}");
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
