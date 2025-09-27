6cdbb278aa041896d89770edcc93e27fef3c070c
Nikita Vlaskin <sinezeleuny@gmail.com>
2025-09-27 19:40:57 +0200

    Project updated with iterators

--- 13_functional_features/minigrep_improved/src/lib.rs
+++ 13_functional_features/minigrep_improved/src/lib.rs
@@ -24,25 +24,29 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
+    pub fn build(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
+        args.next();
 
-        let query = args[1].clone();
-        let file_path = args[2].clone();
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string")
+        };
+        let file_path = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file path")
+        };
 
-        let mut ignore_case = env::var("IGNORE_CASE").is_ok();
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
 
-        if args.len() > 3 {
-            let option = args[3].clone();
-            if option.to_lowercase() == "ignore_case" {
-                ignore_case = true;
-            }
-            if option.to_lowercase() == "mind_case" {
-                ignore_case = false;
-            }
-        }
+        // if args.len() > 3 {
+        //     let option = args[3].clone();
+        //     if option.to_lowercase() == "ignore_case" {
+        //         ignore_case = true;
+        //     }
+        //     if option.to_lowercase() == "mind_case" {
+        //         ignore_case = false;
+        //     }
+        // }
 
         Ok(Config { query, file_path, ignore_case })
     }
@@ -52,24 +56,17 @@ pub fn search<'a>(
     query: &str,
     contents: &'a str,
 ) -> Vec<&'a str> {
-    let mut results = Vec::new();
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-    results
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let query = query.to_lowercase();
-    let mut results = Vec::new();
-    for line in contents.lines() {
-        if line.to_lowercase().contains(&query) {
-            results.push(line);
-        }
-    }
-    results
+    contents
+        .lines()
+        .filter(|line| line.to_lowercase().contains(query))
+        .collect()
 }
 
 #[cfg(test)]
