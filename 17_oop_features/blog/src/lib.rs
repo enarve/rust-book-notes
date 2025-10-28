97a2be4e8b3fecc15072f8eb925bbfb1c6773365
Nikita Vlaskin <sinezeleuny@gmail.com>
2025-10-28 15:26:34 +0100

    Another way to implement logic

--- 17_oop_features/blog/src/lib.rs
+++ 17_oop_features/blog/src/lib.rs
@@ -1,100 +1,146 @@
+// pub struct Post {
+//     state: Option<Box<dyn State>>,
+//     content: String,
+// }
+
+// impl Post {
+//     pub fn new() -> Post {
+//         Post {
+//             state: Some(Box::new(Draft {})),
+//             content: String::new(),
+//         }
+//     }
+
+//     pub fn add_text(&mut self, text: &str) {
+//         let changes = self.state.as_ref().unwrap().changes(text);
+//         self.content.push_str(changes);
+//     }
+
+//     pub fn content(&self) -> &str {
+//         self.state.as_ref().unwrap().content(self)
+//     }
+
+//     pub fn request_review(&mut self) {
+//         if let Some(s) = self.state.take() {
+//             self.state = Some(s.request_review())
+//         }
+//     }
+
+//     pub fn approve(&mut self) {
+//         if let Some(s) = self.state.take() {
+//             self.state = Some(s.approve())
+//         }
+//     }
+
+//     pub fn reject(&mut self) {
+//         if let Some(s) = self.state.take() {
+//             self.state = Some(s.reject())
+//         }
+//     }
+// }
+
+// trait State {
+//     fn request_review(self: Box<Self>) -> Box<dyn State>;
+//     fn approve(self: Box<Self>) -> Box<dyn State>;
+//     fn content<'a>(&self, post: &'a Post) -> &'a str {
+//         ""
+//     }
+//     fn reject(self: Box<Self>) -> Box<dyn State>;
+//     fn changes<'a>(&self, text: &'a str) -> &'a str {
+//         ""
+//     }
+// }
+
+// struct PendingReview {}
+
+// struct Draft {}
+
+// impl State for Draft {
+//     fn request_review(self: Box<Self>) -> Box<dyn State> {
+//         Box::new(PendingReview {})
+//     }
+//     fn approve(self: Box<Self>) -> Box<dyn State> {
+//         self
+//     }
+//     fn reject(self: Box<Self>) -> Box<dyn State> {
+//         self
+//     }
+//     fn changes<'a>(&self, text: &'a str) -> &'a str {
+//         text
+//     }
+// }
+
+// impl State for PendingReview {
+//     fn request_review(self: Box<Self>) -> Box<dyn State> {
+//         self
+//     }
+//     fn approve(self: Box<Self>) -> Box<dyn State> {
+//         Box::new(Published {})
+//     }
+//     fn reject(self: Box<Self>) -> Box<dyn State> {
+//         Box::new(Draft {})
+//     }
+// }
+
+// struct Published {}
+
+// impl State for Published {
+//     fn request_review(self: Box<Self>) -> Box<dyn State> {
+//         self
+//     }
+//     fn approve(self: Box<Self>) -> Box<dyn State> {
+//         self
+//     }
+//     fn content<'a>(&self, post: &'a Post) -> &'a str {
+//         &post.content
+//     }
+//     fn reject(self: Box<Self>) -> Box<dyn State> {
+//         self
+//     }
+// }
+
 pub struct Post {
-    state: Option<Box<dyn State>>,
     content: String,
 }
 
-impl Post {
-    pub fn new() -> Post {
-        Post {
-            state: Some(Box::new(Draft {})),
-            content: String::new(),
-        }
-    }
+pub struct DraftPost {
+    content: String
+}
 
-    pub fn add_text(&mut self, text: &str) {
-        let changes = self.state.as_ref().unwrap().changes(text);
-        self.content.push_str(changes);
+impl Post {
+    pub fn new() -> DraftPost {
+        DraftPost { content: String::new() }
     }
 
     pub fn content(&self) -> &str {
-        self.state.as_ref().unwrap().content(self)
+        &self.content
+    }
+}
+
+impl DraftPost {
+    pub fn add_text(&mut self, text: &str) {
+        self.content.push_str(text);
     }
 
-    pub fn request_review(&mut self) {
-        if let Some(s) = self.state.take() {
-            self.state = Some(s.request_review())
-        }
-    }
-
-    pub fn approve(&mut self) {
-        if let Some(s) = self.state.take() {
-            self.state = Some(s.approve())
-        }
-    }
-
-    pub fn reject(&mut self) {
-        if let Some(s) = self.state.take() {
-            self.state = Some(s.reject())
+    pub fn request_review(self) -> PendingReviewPost {
+        PendingReviewPost {
+            content: self.content
         }
     }
 }
 
-trait State {
-    fn request_review(self: Box<Self>) -> Box<dyn State>;
-    fn approve(self: Box<Self>) -> Box<dyn State>;
-    fn content<'a>(&self, post: &'a Post) -> &'a str {
-        ""
-    }
-    fn reject(self: Box<Self>) -> Box<dyn State>;
-    fn changes<'a>(&self, text: &'a str) -> &'a str {
-        ""
-    }
+pub struct PendingReviewPost {
+    content: String,
 }
 
-struct PendingReview {}
+impl PendingReviewPost {
+    pub fn approve(self) -> Post {
+        Post {
+            content: self.content,
+        }
+    }
 
-struct Draft {}
-
-impl State for Draft {
-    fn request_review(self: Box<Self>) -> Box<dyn State> {
-        Box::new(PendingReview {})
-    }
-    fn approve(self: Box<Self>) -> Box<dyn State> {
-        self
-    }
-    fn reject(self: Box<Self>) -> Box<dyn State> {
-        self
-    }
-    fn changes<'a>(&self, text: &'a str) -> &'a str {
-        text
-    }
-}
-
-impl State for PendingReview {
-    fn request_review(self: Box<Self>) -> Box<dyn State> {
-        self
-    }
-    fn approve(self: Box<Self>) -> Box<dyn State> {
-        Box::new(Published {})
-    }
-    fn reject(self: Box<Self>) -> Box<dyn State> {
-        Box::new(Draft {})
-    }
-}
-
-struct Published {}
-
-impl State for Published {
-    fn request_review(self: Box<Self>) -> Box<dyn State> {
-        self
-    }
-    fn approve(self: Box<Self>) -> Box<dyn State> {
-        self
-    }
-    fn content<'a>(&self, post: &'a Post) -> &'a str {
-        &post.content
-    }
-    fn reject(self: Box<Self>) -> Box<dyn State> {
-        self
+    pub fn reject(self) -> DraftPost {
+        DraftPost { content: self.content }
     }
 }
--- 17_oop_features/blog/src/main.rs
+++ 17_oop_features/blog/src/main.rs
@@ -1,14 +1,24 @@
 use blog::Post;
 
 fn main () {
+    // let mut post = Post::new();
+
+    // post.add_text("I ate a salad for linch today");
+    // assert_eq!("", post.content());
+
+    // post.request_review();
+    // assert_eq!("", post.content());
+
+    // post.approve();
+    // assert_eq!("I ate a salad for linch today", post.content());
+
     let mut post = Post::new();
 
     post.add_text("I ate a salad for linch today");
-    assert_eq!("", post.content());
 
-    post.request_review();
-    assert_eq!("", post.content());
+    let post = post.request_review();
 
-    post.approve();
+    let post = post.approve();
+
     assert_eq!("I ate a salad for linch today", post.content());
 }
