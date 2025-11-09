2fb7537765cf13d0a71a4ee45067302342832973
Nikita Vlaskin <sinezeleuny@gmail.com>
2025-11-09 16:47:48 +0100

    Unsafe use cases

--- 19_advanced_features/unsafe_rust/src/main.rs
+++ 19_advanced_features/unsafe_rust/src/main.rs
@@ -1,4 +1,50 @@
+use std::slice;
+
+// unsafe trait
+unsafe trait Foo {
+    // methods go here
+}
+
+unsafe impl Foo for i32 {
+    // method implementations
+}
+
+// foreign function interface
+unsafe extern "C" {
+    fn abs(input: i32) -> i32;
+}
+
+#[unsafe(no_mangle)]
+pub extern "C" fn call_from_c() {
+    println!("Just called a Rust function from C!");
+}
+
+// global/static variable
+static HELLO_WORLD: &str = "Hello, world!";
+
+static mut COUNTER: u32 = 0;
+
+fn add_to_count(inc: u32) {
+    unsafe {
+        COUNTER += inc;
+    }
+}
+
+// MARK: Main
 fn main() {
+
+    add_to_count(3);
+
+    // unsafe {
+        // println!("COUNTER: {COUNTER}");
+    // }
+
+    println!("value is {HELLO_WORLD}");
+
+    unsafe {
+        println!("Absolute value of -3 according to C: {}", abs(-3));
+    }
+
     unsafe {
         let a: *const i32 = &2;
         println!("{}", *a);
@@ -15,5 +61,41 @@ fn main() {
         println!("r is {}", *r);
         println!("r1 is {}", *r1);
         println!("r2 is {}", *r2);
+    }
+
+    unsafe fn dangerous() {}
+
+    unsafe {
+        dangerous();
+    }
+
+    // creating safe abstraction over unsafe
+    let mut v = vec![1, 2, 3, 4, 5, 6];
+    let r = &mut v[..];
+
+    let (a, b) = r.split_at_mut(3);
+    assert_eq!(a, &mut [1, 2, 3]);
+    assert_eq!(b, &mut [4, 5, 6]);
+
+    // crash example
+    let address = 0x01234usize;
+    let r = address as *mut i32;
+
+    let values: &[i32] = unsafe {
+        slice::from_raw_parts_mut(r, 10000)
+    };
+}
+
+fn split_at_mut(values: &mut [i32], mid: usize) -> (&mut [i32], &mut [i32]) {
+    let len = values.len();
+    let ptr = values.as_mut_ptr();
+
+    assert!(mid <= len);
+
+    unsafe {
+        (
+            slice::from_raw_parts_mut(ptr, mid),
+            slice::from_raw_parts_mut(ptr.add(mid), len - mid)
+        )
     }
 }
