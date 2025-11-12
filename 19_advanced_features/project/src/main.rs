d57e73ea124741ce425869bf3a08e8c0dab9a41a
Nikita Vlaskin <sinezeleuny@gmail.com>
2025-11-12 20:47:33 +0100

    More macros

--- 19_advanced_features/project/src/main.rs
+++ 19_advanced_features/project/src/main.rs
@@ -7,3 +7,16 @@ struct Pancakes;
 fn main() {
     Pancakes::hello_macro();
 }
+
+// Attribute-like macros
+// #[route(GET, "/")]
+// fn index() {}
+
+// #[proc_macro_attribute]
+// pub fn route(_attr: TokenStream, _item: TokenStream) -> TokenStream {}
+
+// Function-like macros
+// let sql = sql!(SELECT * FROM posts WHERE id=1);
+
+// #[proc_macro]
+// pub fn sql(_input: TokenStream) -> TokenStream {}
